a986ec423caa9a7062b517aadc6cb18aa57044e9
kyouryuukunn <akakyouryuu@gmail.com>
2024-04-28 07:31:55 +0900

    up

--- keyboards/keyboard_quantizer/rp/keymaps/default/keymap.c
+++ keyboards/keyboard_quantizer/rp/keymaps/default/keymap.c
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/* 基本的にはNodokaの動作を踏襲しているが、caps + pause でnodokaモードをトグル(デフォルトオン) クリックの左右反転のレイアウトで実施*/
-/* Insertからのpagedowmまでのキーを変換とともに押すとマクロ実行 */
+/* 基本的にはNodokaの動作を踏襲しているが、caps + ESC でnodokaモードをトグル(デフォルトオン) クリックの左右反転のレイアウトで実施*/
+/* InsertからのpagedowmまでのキーをCaps後に押すとマクロ実行 */
 /*  */
 /* DM_REC1, DM_REC2, DM_RSTP */
 /* DM_PLY1, DM_PLY2,     -   */
@@ -22,6 +22,7 @@
 #include "quantum.h"
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
+#include "macro.h"
 
 #include "pointing_device.h"
 #include "debug.h"
@@ -56,6 +57,7 @@ enum custom_keycodes {
     ALT_F4,
     WIN_V,
     OSL_CAPS,
+    NEW_PSCR,
 
     /* FN1_APP, */
     /* FN2_CAPS, */
@@ -90,7 +92,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_NODOKA] =  LAYOUT(
-    KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,     KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,            G(S(KC_S)),   KC_SCROLLLOCK, KC_PAUS,
+    KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,     KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,            NEW_PSCR,   KC_SCROLLLOCK, KC_PAUS,
     JP_ZKHK, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, JP_MINS, JP_CIRC, JP_YEN, KC_BSPACE,         KC_INSERT, KC_HOME, KC_PGUP,       KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
     KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, JP_AT, JP_LBRC,           KC_ENT,           KC_DEL, KC_END,  KC_PGDN,          KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
     OSL_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, JP_SCLN, JP_COLN, JP_RBRC,                                                          KC_KP_4, KC_KP_5, KC_KP_6,
@@ -104,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_HENKAN] =  LAYOUT(
     _______, _______, _______, _______, _______,     KC_SYSTEM_POWER, _______, KC_SYSTEM_SLEEP, _______,     _______, KC_MUTE, KC_VOLD, KC_VOLU,   _______, _______, _______,
-    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______, _______,               DM_REC1, DM_REC2, DM_RSTP,    _______, _______, _______, _______,
-    _______,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, JP_MINS, JP_CIRC,                   JP_YEN,             DM_PLY1, DM_PLY2, _______,    _______, _______, _______, _______,
+    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______, _______,               _______, _______, _______,    _______, _______, _______, _______,
+    _______,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, JP_MINS, JP_CIRC,                   JP_YEN,             _______, _______, _______,    _______, _______, _______, _______,
     _______,   CTRL_A,  NEXTWIN, KC_INS, ALT_F4,  KC_END, KC_LEFT , KC_DOWN, KC_UP, KC_RGHT, KC_PGDN, KC_PGUP, _______ ,                                         _______, _______, _______,
     _______,    CTRL_Z,  CTRL_X,  CTRL_C,  CTRL_V,  KC_HOME, KC_BSPC, KC_DEL, WIN_V, _______, _______, _______, _______,                     _______,            _______, _______, _______, _______,
     _______, _______, _______, _______, SFT_SSPC, KC_APP, _______, _______, _______, _______, _______,                             _______,  _______, _______,   _______,          _______,
@@ -470,12 +472,57 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                         unregister_code16(KC_LGUI);
 			return false;
                         break;
-                    case KC_PAUS: // カスタムキーマップ トグル
+                    case KC_ESC: // カスタムキーマップ トグル
 			caps_pressed = false;
                         layer_invert(_QWERTY);
 			return false;
                         break;
-                }
+                    case NEW_PSCR: // 定型文1
+			caps_pressed = false;
+			SEND_STRING(MACRO1_STRINGS);
+			return false;
+                        break;
+                    case KC_SCROLLLOCK: // 定型文2
+			caps_pressed = false;
+			SEND_STRING(MACRO2_STRINGS);
+			return false;
+                        break;
+                    case KC_PAUSE: // 定型文3
+			caps_pressed = false;
+			SEND_STRING(MACRO3_STRINGS);
+			return false;
+                        break;
+                    case KC_INSERT: // DM_REC1
+			caps_pressed = false;
+                        register_code16(DM_REC1);
+                        unregister_code16(DM_REC1);
+			return false;
+                        break;
+                    case KC_HOME: // DM_REC2
+			caps_pressed = false;
+                        register_code16(DM_REC2);
+                        unregister_code16(DM_REC2);
+			return false;
+                        break;
+                    case KC_PGUP: // DM_RSTP
+			caps_pressed = false;
+                        register_code16(DM_RSTP);
+                        unregister_code16(DM_RSTP);
+			return false;
+                        break;
+                    case KC_DEL: // DM_PLY1
+			caps_pressed = false;
+                        register_code16(DM_PLY1);
+                        unregister_code16(DM_PLY1);
+			return false;
+                        break;
+                    case KC_END: // DM_PLY2
+			caps_pressed = false;
+                        register_code16(DM_PLY2);
+                        unregister_code16(DM_PLY2);
+			return false;
+                        break;
+		}
             }
         }
     }
@@ -822,6 +869,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             } else {
                 unregister_code16(KC_F4);
                 unregister_code16(KC_LALT);
+            }
+            break;
+  	case NEW_PSCR:
+            if (record->event.pressed) {
+                register_code16(KC_LGUI);
+                register_code16(KC_LSFT);
+                register_code16(KC_S);
+            } else {
+                unregister_code16(KC_S);
+                unregister_code16(KC_LSFT);
+                unregister_code16(KC_LGUI);
             }
             break;
   	case WIN_V:
